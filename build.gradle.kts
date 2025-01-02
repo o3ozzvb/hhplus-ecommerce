95498b65ec656933c575a18d8118ef529eaef9b4
oeunkyoung <ou1112@naver.com>
2025-01-02 21:50:20 +0900

    Chore: MockAPI 수정 전 프로젝트 설정 변경 (db config 수정, lombok annotationProcessor dependency 추가)

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.projectlombok:lombok:1.18.28")
-	implementation("org.projectlombok:lombok:1.18.28")
+	annotationProcessor("org.projectlombok:lombok:1.18.28")
 
 	// DB
 	runtimeOnly("com.mysql:mysql-connector-j")
