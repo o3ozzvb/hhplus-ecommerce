545590c29f546f14f2b84067ca75f97e9986bf25
oeunkyoung <ou1112@naver.com>
2025-01-05 17:05:39 +0900

    ✨Feat: ProductInventory 기능 및 단위 테스트 작성  - 재고 차감  - 재고 추가

--- src/main/java/kr/hhplus/be/domain/product/ProductInventory.java
+++ src/main/java/kr/hhplus/be/domain/product/ProductInventory.java
@@ -4,9 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import kr.hhplus.be.support.exception.BusinessException;
+import kr.hhplus.be.support.exception.ErrorCode;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
+@Getter
 @Entity
 public class ProductInventory {
 
@@ -18,4 +22,28 @@ public class ProductInventory {
     private LocalDateTime createdAt;
 
     private LocalDateTime updatedAt;
+
+    public static ProductInventory of(long refProductId,int inventory) {
+        ProductInventory productInventory = new ProductInventory();
+
+        productInventory.refProductId = refProductId;
+        productInventory.inventory = inventory;
+        productInventory.createdAt = LocalDateTime.now();
+        productInventory.updatedAt = LocalDateTime.now();
+
+        return productInventory;
+    }
+
+    /** 재고 차감 */
+    public void deductInventory(int quantity) {
+        if (inventory < quantity) {
+            throw new BusinessException(ErrorCode.INSUFFICIENT_INVENTORY);
+        }
+        this.inventory = this.inventory - quantity;
+    }
+
+    /** 재고 추가 */
+    public void increaseInventory(int quantity) {
+        this.inventory = this.inventory + quantity;
+    }
 }
