6c9900bc20ab74d70da99e3fdefed78a6ad75c25
oeunkyoung <ou1112@naver.com>
2025-01-10 01:15:01 +0900

    Chore: @Id 어노테이션 추가..

--- src/main/java/kr/hhplus/be/domain/user/entity/BalanceHistory.java
+++ src/main/java/kr/hhplus/be/domain/user/entity/BalanceHistory.java
@@ -1,11 +1,16 @@
 package kr.hhplus.be.domain.user.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 import java.time.LocalDateTime;
 
 @Entity
 public class BalanceHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long refUserId;
     private int balance;
