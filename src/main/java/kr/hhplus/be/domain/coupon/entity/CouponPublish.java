fe4378caadab5bb89d96d1d6f2cd65fa9d6e2747
oeunkyoung <ou1112@naver.com>
2025-01-09 23:39:52 +0900

    ✨Feat: CouponPublish 도메인 기능 구현 - 쿠폰 발행, 사용

---
+++ src/main/java/kr/hhplus/be/domain/coupon/entity/CouponPublish.java
@@ -0,0 +1,72 @@
+package kr.hhplus.be.domain.coupon.entity;
+
+import jakarta.persistence.*;
+import kr.hhplus.be.domain.coupon.dto.CouponPublishDTO;
+import kr.hhplus.be.domain.coupon.enumtype.CouponPublishStatus;
+import kr.hhplus.be.support.exception.BusinessException;
+import kr.hhplus.be.support.exception.ErrorCode;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@ToString
+public class CouponPublish {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long refCouponId;
+
+    private Long refUserId;
+
+    private LocalDate publishDate;
+
+    private LocalDate redeemDate;
+
+    private LocalDate validStartDate;
+
+    private LocalDate validEndDate;
+
+    @Enumerated(EnumType.STRING)
+    private CouponPublishStatus status;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    public static CouponPublish publishNow(CouponPublishDTO publishDTO) {
+        CouponPublish couponPublish = new CouponPublish();
+
+        couponPublish.refCouponId = publishDTO.getCouponId();
+        couponPublish.refUserId = publishDTO.getUserId();
+        couponPublish.publishDate = LocalDate.now();
+        couponPublish.validStartDate = publishDTO.getValidStartDate();
+        couponPublish.validEndDate = publishDTO.getValidEndDate();
+        couponPublish.status = CouponPublishStatus.AVAILABLE;
+        couponPublish.createdAt = LocalDateTime.now();
+        couponPublish.updatedAt = LocalDateTime.now();
+
+        return couponPublish;
+    }
+
+    /**
+     * 쿠폰 사용
+     */
+    public void redeem() {
+        if (!this.status.equals(CouponPublishStatus.AVAILABLE)) {
+            throw new BusinessException(ErrorCode.COUPON_NOT_AVAILABLE);
+        }
+        if (this.validEndDate.compareTo(LocalDate.now()) < 0) {
+            throw new BusinessException(ErrorCode.COUPON_VALID_DATE_EXPIRED);
+        }
+            this.redeemDate = LocalDate.now();
+        this.status = CouponPublishStatus.REDEEMED;
+        this.updatedAt = LocalDateTime.now();
+    }
+}
