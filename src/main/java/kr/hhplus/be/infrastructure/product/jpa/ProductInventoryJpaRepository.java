72c8502394874a5d13d99e5f7c156fef9e2e5158
oeunkyoung <ou1112@naver.com>
2025-01-13 22:28:38 +0900

    🐛Fix: package 변경 과정에서 변경 누락된 import 문 수정

--- src/main/java/kr/hhplus/be/infrastructure/product/jpa/ProductInventoryJpaRepository.java
+++ src/main/java/kr/hhplus/be/infrastructure/product/jpa/ProductInventoryJpaRepository.java
@@ -1,7 +1,7 @@
 package kr.hhplus.be.infrastructure.product.jpa;
 
 import kr.hhplus.be.domain.product.entity.ProductInventory;
-import kr.hhplus.be.infrastructure.product.ProductInventoryRepositoryCustom;
+import kr.hhplus.be.infrastructure.product.custom.ProductInventoryRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductInventoryJpaRepository extends JpaRepository<ProductInventory, Long>, ProductInventoryRepositoryCustom {
