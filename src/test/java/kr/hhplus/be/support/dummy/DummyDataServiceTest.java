6e8885c045a26984fdfb88498c81f225459ab1e0
oeunkyoung <ou1112@naver.com>
2025-02-13 19:24:25 +0900

    ✨Feat: 더미 데이터 세팅을 위한 설정 및 서비스 작성

---
+++ src/test/java/kr/hhplus/be/support/dummy/DummyDataServiceTest.java
@@ -0,0 +1,23 @@
+package kr.hhplus.be.support.dummy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.sql.SQLException;
+
+@ActiveProfiles("dummy")
+@SpringBootTest
+class DummyDataServiceTest {
+
+    @Autowired
+    DummyDataService dummyDataService;
+
+    @Test
+    @Commit
+    void initializeData() throws InterruptedException, SQLException {
+        dummyDataService.initializeData();
+    }
+}
